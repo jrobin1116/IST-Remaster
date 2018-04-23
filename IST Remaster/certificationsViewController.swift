96e058c687b3cb345d5993468ae66789c60b33bb
IST <ist@ists-mac-mini-6.local>
2018-04-23 11:01:06 -0500

    newest 4/23

--- IST Remaster/certificationsViewController.swift
+++ IST Remaster/certificationsViewController.swift
@@ -21,15 +21,7 @@ class certificationsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    @IBAction func openWebsite(_ sender: Any) {
+        UIApplication.shared.open(URL(string:"https://certification.comptia.org/docs/default-source/downloadablefiles/it-certification-roadmap.pdf")! as URL, options: [:], completionHandler: nil)    }
 
 }
